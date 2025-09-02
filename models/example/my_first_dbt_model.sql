64aadbe64b35b6e3d6a191ce4415a1733c0b92cd
Valerie Habbel <valerie.habbel@xebia.com>
2025-09-02 08:08:44 +0000

    Created the source file and our first dbt model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,27 +1,5 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
+-- Use the `ref` function to select from other models
 
 select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+from {{ source('dbt_vhabbel', 'dim_customers') }}
